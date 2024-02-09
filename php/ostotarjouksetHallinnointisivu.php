bbb738719fab68edfa7d81b4db09732cbdb6d8aa
Mikko Talikka <mikko.talikka@student.hamk.fi>
2024-02-09 16:38:59 +0200

    Muokattu hallinnointisivua niin, että tietokannasta tuleva data menee taulokkomuotoon.

--- php/ostotarjouksetHallinnointisivu.php
+++ php/ostotarjouksetHallinnointisivu.php
@@ -17,7 +17,7 @@
         </ul>
     </nav>
     <main>
-        <?php
+    <?php
             mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_INDEX);
 
             $initials=parse_ini_file("./.ht.asetukset.ini");
@@ -26,20 +26,33 @@
                 $yhteys=mysqli_connect($initials["palvelin"], $initials["tunnus"] , $initials["pass"], $initials["tk"]);
             }
             catch(Exception $e){
-                header("Location:../pages/yhteysvirhe.html");
+                header("Location:./yhteysvirhe.html");
                 exit;
             }
 
             $tulos=mysqli_query($yhteys, "select * from ostotarjoukset");
-            while ($rivi=mysqli_fetch_object($tulos)){
-                print "<p>$rivi->id. $rivi->rekisterinumero, $rivi->puhelinnumero, $rivi->email, $rivi->kokonimi, $rivi->raha, $rivi->kilometrilukema, $rivi->lisatieto, $rivi->kuva, $rivi->kasittelyntila ".
-                "<a href='./katso.php?katsottava=$rivi->id'>Katso</a> " .
-                "<a href='./muokkaa.php?muokattava=$rivi->id'>Muokkaa</a> " . 
-                "<a href='./poista.php?poistettava=$rivi->id'>Poista</a></p>";
+            print "<table border='1'>";
+            print "<tr><th>ID</th><th>Rekisterinumero</th><th>Puhelinnumero</th><th>Sähköposti</th><th>Kokonimi</th><th>Raha</th>
+            <th>Kilometrilukema</th><th>Lisätieto</th><th>Kuva</th><th>Käsittelyntila</th><th>Toiminnot</th></tr>";
+            while ($rivi = mysqli_fetch_object($tulos)) {
+                print "<tr>";
+                print "<td>$rivi->id</td>";
+                print "<td>$rivi->rekisterinumero</td>";
+                print "<td>$rivi->puhelinnumero</td>";
+                print "<td>$rivi->email</td>";
+                print "<td>$rivi->kokonimi</td>";
+                print "<td>$rivi->raha</td>";
+                print "<td>$rivi->kilometrilukema</td>";
+                print "<td>$rivi->lisatieto</td>";
+                print "<td>$rivi->kuva</td>";
+                print "<td>$rivi->kasittelyntila</td>";
+                print "<td><a href='./katso.php?katsottava=$rivi->id'>Katso</a> | <a href='./muokkaa.php?muokattava=$rivi->id'>Muokkaa</a> | 
+                <a href='./poista.php?poistettava=$rivi->id'>Poista</a></td>";
+                print "</tr>";
             }
+            print "</table>";
             mysqli_close($yhteys);
         ?>
     </main>
-    
 </body>
 </html>
